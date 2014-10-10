8c7763d8d567a38146bb081eaf6afd513cdbeb13
Connor Bode <connor.bode@gmail.com>
2014-10-10 14:42:05 -0400

    fix tests

--- app/index/postings_list_spec.rb
+++ app/index/postings_list_spec.rb
@@ -3,8 +3,8 @@ require_relative './postings_list.rb'
 describe 'Postings List' do
 
   it 'should add a doc' do
-    p = PostingsList.new 1
-    p.add "Hello", 2
+    p = PostingsList.new
+    p.add "Hello"
     expect(p.list.length).to eq 1
   end
 end
