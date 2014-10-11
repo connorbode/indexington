03daeef9872d2a223d0feb9548ffb7b4c59cc00a
Connor Bode <connor.bode@gmail.com>
2014-10-11 16:14:19 -0400

    merge postings lists

--- app/index/merger.rb
+++ app/index/merger.rb
@@ -23,61 +23,85 @@ class Merger
     dict = File.open @options[:destination] + '.dict', 'w'
     post = File.open @options[:destination] + '.post', 'w'
     @sources.each { |source| source[:term] = get_next_term(source) }
+
     loop do
-      @sources.sort_by! { |source| [source[:term][:term], source[:term][:doc_id]] }
-      i = 0
-      current_term = @sources[0][:term][:term]
-      dict.print "#{@sources[0][:term][:term]}:#{@sources[0][:term][:doc_id]}"
-      @sources[0][:term] = get_next_term(@sources[0])
-      @sources.delete_at 0 if @sources[0][:term].nil?
-      loop do 
-        if not @sources[i+1].nil? and current_term == @sources[i+1][:term][:term] then
-          dict.print ",#{@sources[i+1][:term][:doc_id]}"
-          @sources[i+1][:term] = get_next_term(@sources[i+1])
-          if @sources[i+1][:term].nil? then
-            @sources.delete_at (i+1)
-          else
-            i += 1
-          end
-        else
-          break
-        end
-      end
-      dict.print ";"
-      break if @sources.length == 0
+      @sources.sort_by { |source| source[:term] }
+      this_term = [@sources[0]]
+      postings_list = get_next_postings_list @sources[0]
+      dict.print @sources[]
     end
+
     dict.close
+  end
+
+  # reads from postings lists in sources
+  # writes merged postings list to post
+  # returns number of characters written
+  def write_postings_list post, sources
+    postings_lists = sources.map { |source| { list: source[:postings_lists], more: true } }
+    # byebug
+    postings_lists.each { |list| list[:next_post] = get_next_post list[:list] }
+
+    loop do
+      postings_lists.sort_by { |list| list[:next_post][:post] }
+      post.print "#{postings_lists[0][:next_post][:post]}"
+      if postings_lists[0][:next_post][:last] then
+        postings_lists.delete_at 0 
+      else 
+        postings_lists[0][:next_post] = get_next_post postings_lists[0][:list]
+      end
+      break if postings_lists.empty?
+      post.print ","
+    end
+    post.print ";"
+  end
+
+  # gets the next post from a postings list
+  # sets a flag if it is the last
+  def get_next_post source
+    post = ""
+    last = false
+    loop do
+      c = source.gets 1
+      break if c == ","
+      if c == ";" then
+        last = true
+        break
+      end
+      post << c
+    end
+    return { post: post.to_i, last: last }
   end
 
   # retrieves the next term from a source
   def get_next_term s
     term = ""
-    doc_id = ""
+    post_ptr = ""
     loop do
       char = s[:dictionary].gets 1
       return nil if char.nil?
       break if char == ":"
-      term += char
+      term << char
     end
     loop do
       char = s[:dictionary].gets 1
       return nil if char.nil?
       break if char == ";"
-      doc_id += char
+      post_ptr << char
     end
-    return {term: term, doc_id: doc_id}
+    return {term: term, post_ptr: post_ptr}
   end
 
-  # retrieves the next postings list from a source
-  def get_next_postings_list s
-    list = ""
-    loop do
-      char = s[:postings_lists].gets 1
-      return nil if char.nil?
-      break if char == ";"
-      list << char
-    end
-    return list
-  end
+  # # retrieves the next postings list from a source
+  # def get_next_postings_list s
+  #   list = []
+  #   loop do
+  #     char = s[:postings_lists].gets 1
+  #     return nil if char.nil?
+  #     break if char == ";"
+  #     list << char
+  #   end
+  #   return list
+  # end
 
 end
--- app/index/merger_spec.rb
+++ app/index/merger_spec.rb
@@ -7,33 +7,59 @@ describe 'Merger' do
       i0 = File.expand_path 'spec/fixtures/i0.dict'
       d = { dictionary: File.open(i0) }
       m = Merger.new destination: i0, sources: []
-      expect(m.get_next_term d).to eq({term: 'afg', doc_id: '0'})
-      expect(m.get_next_term d).to eq({term: 'against', doc_id: '8'})
-      expect(m.get_next_term d).to eq({term: 'agenc', doc_id: '12'})
+      expect(m.get_next_term d).to eq({term: 'afg', post_ptr: '0'})
+      expect(m.get_next_term d).to eq({term: 'against', post_ptr: '8'})
+      expect(m.get_next_term d).to eq({term: 'agenc', post_ptr: '12'})
       expect(m.get_next_term d).to eq(nil)
     end
   end
 
-  describe 'get_next_postings_list' do
-    it 'gets the next postings list' do
-      p0 = File.expand_path 'spec/fixtures/i0.post'
-      i = { postings_lists: File.open(p0) }
-      m = Merger.new destination: '', sources: []
-      expect(m.get_next_postings_list i).to eq '12,162'
-      expect(m.get_next_postings_list i).to eq '155'
-      expect(m.get_next_postings_list i).to eq '48'
+  describe 'get_next_post' do
+    it 'gets the next post' do
+      i0 = File.expand_path 'spec/fixtures/i0.post'
+      s = File.open i0
+      m = Merger.new destination: i0, sources: []
+      expect(m.get_next_post s).to eq({post: 12, last: false})
+      expect(m.get_next_post s).to eq({post: 162, last: true})
+      expect(m.get_next_post s).to eq({post: 155, last: true})
+      expect(m.get_next_post s).to eq({post: 48, last: true})
     end
   end
 
-  describe 'merge' do
-    it 'merges two files' do
-      dest = File.expand_path 'spec/fixtures/dest'
-      i0 = File.expand_path 'spec/fixtures/i0'
-      i1 = File.expand_path 'spec/fixtures/i1'
-      m = Merger.new destination: dest, sources: [i0, i1]
-      m.merge
-      d = File.open(dest + '.dict').read
-      expect(d).to eq "afg:0;against:8;agenc:0,12;agricultur:4;ahead:8;"
+  describe 'write_postings_list' do
+    it 'merges two postings lists' do
+      p0 = File.open(File.expand_path 'spec/fixtures/i0.post')
+      p1 = File.open(File.expand_path 'spec/fixtures/i1.post')
+      dest = File.open(File.expand_path('spec/fixtures/dest.post'), 'w')
+      m = Merger.new destination: '', sources: []
+      m.write_postings_list dest, [{postings_lists: p0}, {postings_lists: p1}]
+      dest.close
+      dest = File.open(File.expand_path('spec/fixtures/dest.post'))
+      r = dest.read
+      expect(r).to eq '12,162,194;'
     end
   end
+
+  # describe 'get_next_postings_list' do
+  #   it 'gets the next postings list' do
+  #     p0 = File.expand_path 'spec/fixtures/i0.post'
+  #     i = { postings_lists: File.open(p0) }
+  #     m = Merger.new destination: '', sources: []
+  #     expect(m.get_next_postings_list i).to eq '12,162'
+  #     expect(m.get_next_postings_list i).to eq '155'
+  #     expect(m.get_next_postings_list i).to eq '48'
+  #   end
+  # end
+
+  # describe 'merge' do
+  #   it 'merges two files' do
+  #     dest = File.expand_path 'spec/fixtures/dest'
+  #     i0 = File.expand_path 'spec/fixtures/i0'
+  #     i1 = File.expand_path 'spec/fixtures/i1'
+  #     m = Merger.new destination: dest, sources: [i0, i1]
+  #     m.merge
+  #     d = File.open(dest + '.dict').read
+  #     expect(d).to eq "afg:0;against:8;agenc:0,12;agricultur:4;ahead:8;"
+  #   end
+  # end
 end
