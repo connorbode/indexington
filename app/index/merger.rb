bbad1ae3fdfd3e4268b16a563d2545668936919c
Connor Bode <connor.bode@gmail.com>
2014-10-11 15:07:10 -0400

    changes. CLASSIC

--- app/index/merger.rb
+++ app/index/merger.rb
@@ -4,43 +4,68 @@
 # options
 # - dest_dir : the directory where the final index should be put
 
+require 'byebug'
+
 class Merger
 
   # initializes the merger
   def initialize options
     @options = options
-    @sources = @options[:sources].map { |source| File.open source }
+    @sources = @options[:sources].map do |source| 
+      { dictionary: File.open(source + '.dict'),
+        postings_lists: File.open(source + '.post') }
+    end
+  end
+
+  # merges sources into destination
+  # this algo is f*$%ed
+  def merge
+    dict = File.open @options[:destination] + '.dict', 'w'
+    post = File.open @options[:destination] + '.post', 'w'
+    @sources.each { |source| source[:term] = get_next_term(source) }
+    loop do
+      @sources.sort_by! { |source| [source[:term][:term], source[:term][:doc_id]] }
+      i = 0
+      current_term = @sources[0][:term][:term]
+      dict.print "#{@sources[0][:term][:term]}:#{@sources[0][:term][:doc_id]}"
+      @sources[0][:term] = get_next_term(@sources[0])
+      @sources.delete_at 0 if @sources[0][:term].nil?
+      loop do 
+        if not @sources[i+1].nil? and current_term == @sources[i+1][:term][:term] then
+          dict.print ",#{@sources[i+1][:term][:doc_id]}"
+          @sources[i+1][:term] = get_next_term(@sources[i+1])
+          if @sources[i+1][:term].nil? then
+            @sources.delete_at (i+1)
+          else
+            i += 1
+          end
+        else
+          break
+        end
+      end
+      dict.print ";"
+      break if @sources.length == 0
+    end
+    dict.close
   end
 
   # retrieves the next term from a source
-  def get_next_term source
+  def get_next_term s
     term = ""
     doc_id = ""
     loop do
-      char = source.gets 1
+      char = s[:dictionary].gets 1
       return nil if char.nil?
       break if char == ":"
       term += char
     end
     loop do
-      char = source.gets 1
+      char = s[:dictionary].gets 1
       return nil if char.nil?
       break if char == ";"
       doc_id += char
     end
     return {term: term, doc_id: doc_id}
-  end
-
-  # checks whether the destination index already exists
-  # creates a new one if it does
-  def check_destination
-    if File.exists? @options[:destination] then
-      random_number = rand(10000)
-      @tmp = "#{@options[:destination]}.#{random_number}.tmp"
-      @sources.push { |source| File.open @tmp }
-      FileUtils.copy_file @options[:destination], @tmp
-    end
-    return @tmp
   end
 
 end
--- app/index/merger_spec.rb
+++ app/index/merger_spec.rb
@@ -2,26 +2,27 @@ require_relative './merger.rb'
 
 describe 'Merger' do
 
-  describe 'check_destination' do 
-    it 'copies the destination to a new file if it exists' do
-      dict0 = File.expand_path 'spec/fixtures/dict0'
-      m = Merger.new destination: dict0, sources: []
-      tmp = m.check_destination
-      expect(tmp.class.name).to eq "String"
-      expect(File.exists? tmp).to eq true
-      File.delete tmp
-    end
-  end
-
   describe 'get_next_term' do
     it 'gets the next term properly' do
-      dict0 = File.expand_path 'spec/fixtures/dict0'
-      d = File.open dict0
-      m = Merger.new destination: dict0, sources: []
+      i0 = File.expand_path 'spec/fixtures/i0.dict'
+      d = { dictionary: File.open(i0) }
+      m = Merger.new destination: i0, sources: []
       expect(m.get_next_term d).to eq({term: 'afg', doc_id: '0'})
       expect(m.get_next_term d).to eq({term: 'against', doc_id: '8'})
       expect(m.get_next_term d).to eq({term: 'agenc', doc_id: '12'})
       expect(m.get_next_term d).to eq(nil)
+    end
+  end
+
+  describe 'merge' do
+    it 'merges two files' do
+      dest = File.expand_path 'spec/fixtures/dest'
+      i0 = File.expand_path 'spec/fixtures/i0'
+      i1 = File.expand_path 'spec/fixtures/i1'
+      m = Merger.new destination: dest, sources: [i0, i1]
+      m.merge
+      d = File.open(dest + '.dict').read
+      expect(d).to eq "afg:0;against:8;agenc:0,12;agricultur:4;ahead:8;"
     end
   end
 end
