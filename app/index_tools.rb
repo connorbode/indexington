4b5eef56c4dac562e7860e2880f2e45d7b79a8cc
Connor Bode <connor.bode@gmail.com>
2014-10-17 19:01:59 -0400

    tools to get index stats

---
+++ app/index_tools.rb
@@ -0,0 +1,33 @@
+
+if not ARGV[0] then
+  puts ""
+  puts "-----------------------TOOOLZZZ---------------------"
+  puts "Usage: 'ruby index_tools.rb {INDEX}'"
+  puts "-----------------------TOOOLZZZ---------------------"
+  puts ""
+
+else
+
+  index_path = File.expand_path(ARGV[0])
+  dict = File.open(index_path + ".dict")
+  post = File.open(index_path + ".post")
+
+  # count terms
+  terms = 0
+  loop do
+    char = dict.gets 1
+    break if char.nil?
+    terms += 1 if char == ";"
+  end
+
+  # count postings
+  postings = 0
+  loop do
+    char = post.gets 1
+    break if char.nil?
+    postings += 1 if char == ";" or char == ","
+  end
+
+  puts "Dictionary contains #{terms} unique terms"
+  puts "Postings file contains #{postings} postings"
+end
