fe462949aaa7968d8f4e53cc4c76c234955e4b80
Connor Bode <connor.bode@gmail.com>
2014-11-08 20:56:47 -0500

    read in document frequency

--- app/index/index.rb
+++ app/index/index.rb
@@ -8,8 +8,17 @@ class Index
   def initialize options
     @options = options
     @dictionary = load_dictionary
+    @index_meta = load_meta
     @postings_lists = open_postings_lists
     @tokenizer = Tokenizer.new @options[:tokenizer]
+  end
+
+  # loads meta-data into memory
+  def load_meta
+    meta = File.open(@options[:index] + '.meta')
+    num_docs = meta.gets
+    collection_length = meta.gets
+    return {:number_of_documents => num_docs.to_i, :collection_length => collection_length.to_i}
   end
 
   # loads the dictionary into memory
@@ -19,6 +28,7 @@ class Index
     loop do
       term = ""
       position = ""
+      count = ""
       char = ""
       loop do
         char = dict.gets 1
@@ -27,11 +37,16 @@ class Index
       end
       loop do
         char = dict.gets 1
-        break if char.nil? or char == ";"
+        break if char.nil? or char == ":"
         position << char
       end
+      loop do
+        char = dict.gets 1
+        break if char.nil? or char == ";"
+        count << char
+      end
       break if char.nil?
-      dictionary[term] = position
+      dictionary[term] = {:position => position.to_i, :count => count.to_i}
     end
     dict.close
     return dictionary
@@ -67,16 +82,25 @@ class Index
     postings = []
     started = false
     terms.each do |term|
-      post_pointer = @dictionary[term]
+      puts "#{term} idf: #{idf term}"
+      post_pointer = @dictionary[term][:position]
       if not post_pointer.nil? then 
         if postings.empty? then
-          postings = get_postings_list(post_pointer.to_i) if not started
+          postings = get_postings_list(post_pointer) if not started
         else
-          postings = postings & get_postings_list(post_pointer.to_i)
+          postings = postings & get_postings_list(post_pointer)
         end
       end
       started = true
     end
     return postings.sort.uniq
+  end
+
+  # generates idf for a term
+  def idf term
+    total_docs = @index_meta[:number_of_documents]
+    document_frequency = @dictionary[term][:count]
+    return 0 if document_frequency.nil?
+    return Math.log10(total_docs / document_frequency)
   end
 end
